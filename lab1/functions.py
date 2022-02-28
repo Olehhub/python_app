a6fba769bd69e4f6eb13ba7c65b81ff7bebc44f7
O <olaph22@gmail.com>
2022-02-28 19:07:11 +0200

    added some functions

--- lab1/functions.py
+++ lab1/functions.py
@@ -2,8 +2,91 @@ def union(a, b):
     a = list(a)
     b = list(b)
     c = b.copy()
-    for i in range(len(a)):
-        if a[i] not in c:
-            c.append(a[i])
+    for j in a:
+        q = False
+        for i in b:
+            if j == i:
+                q = True
+        if q == False:
+            c.append(j)
+
+    if len(c) <= 1:
+        return c
+    else:
+        for iter_num in range(len(c) - 1, 0, -1):
+            for idx in range(iter_num):
+                if c[idx] > c[idx + 1]:
+                    temp = c[idx]
+                    c[idx] = c[idx + 1]
+                    c[idx + 1] = temp
+
     return c
 
+def crossing(a, b):
+    a = list(a)
+    b = list(b)
+    c = []
+    for j in a:
+        for i in b:
+            if j == i:
+                c.append(j)
+
+    if len(c) <= 1:
+        return c
+    else:
+        for iter_num in range(len(c) - 1, 0, -1):
+            for idx in range(iter_num):
+                if c[idx] > c[idx + 1]:
+                    temp = c[idx]
+                    c[idx] = c[idx + 1]
+                    c[idx + 1] = temp
+    return c
+
+
+def diff(a, b):
+    a = list(a)
+    b = list(b)
+    c = a.copy()
+    for j in a:
+        q = False
+        for i in b:
+            if j == i:
+                q = True
+        if q == True:
+            c.remove(j)
+
+    if len(c) <= 1:
+        return c
+    else:
+        for iter_num in range(len(c) - 1, 0, -1):
+            for idx in range(iter_num):
+                if c[idx] > c[idx + 1]:
+                    temp = c[idx]
+                    c[idx] = c[idx + 1]
+                    c[idx + 1] = temp
+    return c
+
+def symetricdiff(a, b):
+    a = list(a)
+    b = list(b)
+    c = []
+    for j in range(len(a)):
+        if a[j] not in b:
+            c.append(a[j])
+    for i in range(len(b)):
+        if b[i] not in a:
+            c.append(b[i])
+
+    if len(c) <= 1:
+        return c
+    else:
+        for iter_num in range(len(c) - 1, 0, -1):
+            for idx in range(iter_num):
+                if c[idx] > c[idx + 1]:
+                    temp = c[idx]
+                    c[idx] = c[idx + 1]
+                    c[idx + 1] = temp
+    return c
+
+
+print(symetricdiff([3,1,2, 2,5,6,7,7,3,12,12,1], [1,2]))
